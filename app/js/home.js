513210dd2edc6622b2a3351ff6a57c0d049ac934
Luke Deen Taylor <luke@deentaylor.com>
2016-12-12 18:59:36 -0500

    Scroll effect

--- app/js/home.js
+++ app/js/home.js
@@ -1,6 +1,19 @@
 window.addEventListener('scroll', () => {
   const scroll = window.scrollY;
+  const height = window.innerHeight;
+
   // Top bar color
   document.getElementById('top-bar').className =
     scroll > 50 ? 'opaque' : '';
+
+  // Name zoom and fade
+  const name = document.querySelector('h1.huge');
+  name.style.transform = `scale(${(1 + (scroll / window.innerHeight))})`;
+  const opacity = 1 - (2 * (scroll / height));
+  if (opacity <= 0) {
+    name.style.display = 'none';
+  } else {
+    name.style.display = 'block';
+    name.style.opacity = 1 - (2 * (scroll / height));
+  }
 });

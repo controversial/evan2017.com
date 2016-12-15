31d7f19b2318f840761ed72d330ec529b5819750
Luke Deen Taylor <luke@deentaylor.com>
2016-12-15 17:26:43 -0500

    Remove name scroll effect

--- app/js/home.js
+++ app/js/home.js
@@ -5,15 +5,4 @@ window.addEventListener('scroll', () => {
   // Top bar color
   document.getElementById('top-bar').className =
     scroll > 50 ? 'opaque' : '';
-
-  // Name zoom and fade
-  const name = document.querySelector('h1.huge');
-  name.style.transform = `scale(${(1 + (scroll / window.innerHeight))})`;
-  const opacity = 1 - (2 * (scroll / height));
-  if (opacity <= 0) {
-    name.style.display = 'none';
-  } else {
-    name.style.display = 'block';
-    name.style.opacity = 1 - (2 * (scroll / height));
-  }
 });

16d38f6177716cc2c0422a921ccc3f2aeed0489e
Luke Deen Taylor <luke@deentaylor.com>
2017-05-24 20:42:27 -0400

    Last minute changes
    Add about section; remove pledge vote functionality.

--- app/js/home.js
+++ app/js/home.js
@@ -38,18 +38,54 @@ function pledgeVote() {
 
 const message = `
 
-<h3> Pledge your vote </h3>
-<p> Support Evan Holland-Shepler and Luke Taylor for the 2017 junior class president and vice president </p>
+<h3> About </h3>
+<p class="center"> Support Evan Holland-Shepler and Luke Taylor for the 2017 junior class president and vice president </p>
 
-<div class="form">
-  <div class="horizontal-inputs">
-    <input id="first" type="text" placeholder="First Name"> <input id="last" type="text" placeholder="Last Name">
-  </div>
+<hr>
 
-  <input id="email" type="email" placeholder="Email">
+<h4> Prom </h4>
 
-  <div class="button"> <button type="button" id="formSubmit"> Pledge your vote </button> </div>
-</div>
+<p>
+Prom is a highlight of junior year. And in order to have a great prom, we have to cater to <i>your</i>
+wishes. In order to truly create a great prom, we will be letting <b>YOU</b> make the decisions for
+prom—not us. Let us do the organization, but you'll be able to decide everything that will make your
+prom a magical and memorable experience. Through Instagram, Snapchat, and Twitter, we will conduct polls
+to get the class's opinion on every decision.
+</p>
+<p>
+Second, we want prom to be as affordable as possible so that nobody has to struggle to pay for prom tickets.
+Ticket cost should never restriction on a student's prom experience, but we will not sacrifice
+extravagance for cost. Rather than holding a cheap prom, we will fundraise the hell out of prom! With the
+cooperation of the student body, we can truly make prom a magical experience without compromise. We will
+talk to the administration about creating a monthly bake sale for prom, and email various fundraising
+companies to set up candy and cookie dough fundraisers. With the class's help, we will be able to achieve
+our goal of an affordable prom, so that every member of the junior class can have an unrestricted prom
+experience.
+</p>
+<p>
+We can create the best prom New Paltz High School has ever seen.
+</p>
+
+<h4> Dress Code </h4>
+
+<p>
+Luke Taylor and I agree that people are not a distraction! People should be able to express
+themselves however they like within some guidelines. When student government meets to discuss the
+dress code we will both be there fighting for people’s right to express themselves.
+</p>
+
+<h4> Lunch </h4>
+
+<p>
+Look, we know it may not make a difference, but Luke and I will fight tooth and nail to bring change to
+this outrageously short lunch schedule which leaves hardly any time to eat and have a break from class.
+We will set up as many meetings with student government to draft petitions and organize talks with the
+administration to increase lunch times. Our hard-working student body deserves sufficient time to eat
+and socialize.
+</p>
+
+<p>
+</p>
 `;
 
 document.addEventListener('DOMContentLoaded', () => {
